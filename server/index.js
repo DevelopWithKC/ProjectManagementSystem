df70c4040ca3fb481d2a43debbca65093d4b9d69
DevelopWithKC <kpchavan2000@gmail.com>
2022-12-09 15:34:32 +0530

    Application is ready to use

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
-const express = require("express");
-const colors = require("colors");
-// const cors = require("cors");
-
-
-require("dotenv").config();
-const { graphqlHTTP } = require("express-graphql");
-
-
-const schema = require("./schema/schema");
-const connectDB = require("./config/db");
+const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -16,13 +12,13 @@ const app = express();
 // Connect to database
 connectDB();
 
-// app.use(cors());
+app.use(cors());
 
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === "development",
+    graphiql: process.env.NODE_ENV === 'development',
   })
 );
 
